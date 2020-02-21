b2a9e87d62373282c583e59b04ac749ec328ef21
Risky Muaji (@kosan) <muaji@qmuaji.com>
2020-02-21 14:22:32 +0700

    ok

--- core/dbConnect.php
+++ core/dbConnect.php
@@ -1,5 +1,5 @@
 <?php
 
 $connError = 'Maaf, sistem kami sedang dalam perbaikan..';
-mysql_connect('localhost', 'root', 'password') or die($connError);
+mysql_connect('localhost', 'root', 'password2') or die($connError);
 mysql_select_db('reservasi') or die($connError);
