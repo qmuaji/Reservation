fad8c81a42d7a30688cb43eda939ef106db29f30
Risky Muaji <muaji.risky@outlook.com>
2020-02-21 11:54:19 +0700

    ok
    ok

--- core/dbConnect.php
+++ core/dbConnect.php
@@ -1,10 +1,5 @@
 <?php
 
 $connError = 'Maaf, sistem kami sedang dalam perbaikan..';
-mysql_connect('localhost', 'qmuajico_root', 'Muaji*19#') or die($connError);
-mysql_select_db('qmuajico_reservasi') or die($connError);
-
-// versi idHostinger
-// $connError = 'Maaf, sistem kami sedang dalam perbaikan..';
-// mysql_connect('127.0.0.1', 'u426975594_lans', 'password') or die($connError);
-// mysql_select_db('u426975594_lans') or die($connError);
+mysql_connect('localhost', 'root', '') or die($connError);
+mysql_select_db('reservasi') or die($connError);
